bf08771e9a6431b7ec8e7cdfbd0479a970bf0e7e
Joshua Barchenger <joshua.barchenger@protonmail.com>
2021-05-04 10:58:30 -0500

    fix maybe

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.json({ extended: true }));
